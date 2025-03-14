9bea9af22107855d2b25e5384b0328c05c73f494
Rithiga Sri B <93427256+Rithigasri@users.noreply.github.com>
2025-03-14 11:29:50 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-from pywebio.input import textarea, input
+from pywebio.input import textarea, input, input_group, select, NUMBER, actions
 from pywebio import start_server
 from pywebio.output import put_text
 from fpdf import FPDF
@@ -20,34 +20,34 @@ def app_main():
                            buttons=[{'label': 'Yes', 'value': True},
                                     {'label': 'No', 'value': False}])
 
+    text_info = input_group('Text Fonts and Size', [
+        select(label='Select your font', options=FONTS, value='Arial', name='font'),
+        input("Select your text size", value='14', type=NUMBER, name='size')
+    ])
+
     save_location = input("What is the name of your PDF file?")
-    create_pdf(pages, font='Arial', size=16, save_location=save_location)
+    create_pdf(pages, text_info, save_location)
     put_text("Congratulations! A PDF file is generated for you.")
 
 
 def create_page(pdf, text: str, font: str, size: int):
     pdf.add_page()
-    pdf.set_font('arial', '', 16)
+    pdf.set_font(font, '', size)  # Use the font and size provided
 
     lines = text.split('\n')
     for i, sent in enumerate(lines):
         pdf.cell(40, 10, sent, 0, i + 1)
 
 
-text_info = input_group('Text Fonts and Size', [
-    select(label='Select your font', options=FONTS, value='Arial', name='font'),
-    input("Select your text size", value='14', type=NUMBER, name='size')
-])
-
-
-def create_pdf(pages, text_info['font'], text_info['size'], save_location):
+def create_pdf(pages, text_info, save_location):
     pdf = FPDF()
+    font = text_info['font']
+    size = int(text_info['size'])  # Convert to integer
 
     for page in pages:
         create_page(pdf, page, font, size)
 
     pdf.output(save_location, 'F')
-
 
 if __name__ == '__main__':
     start_server(app_main, port=8080, debug=True)
